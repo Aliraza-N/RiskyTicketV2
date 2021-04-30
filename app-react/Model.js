37853254390ec823462f9d917ab78dfe69ace5d3
Aliraza <aliraza.n@live.com>
2021-04-30 16:05:17 -0400

    make request to api

--- app-react/Model.js
+++ app-react/Model.js
@@ -5,6 +5,8 @@ import { StyleSheet, Text, View } from 'react-native';
 const DEFAULT_LAT = '43.643750';
 const DEFAULT_LON = '-79.389983';
 
+const URL = 'http://127.0.0.1:3000/predict'
+
 const Model = () => {
     const [location, setLocation] = useState([DEFAULT_LAT, DEFAULT_LON]);
     const [currentTime, setCurrentTime] = useState('');
@@ -14,6 +16,25 @@ const Model = () => {
         const now = new Date();
         const formattedTime = `${now.getHours()}:${now.getMinutes()}`;
         setCurrentTime(formattedTime);
+    }
+
+    const makePredictionRequest = async () => {
+        await fetch(URL, {
+            method: 'POST',
+            headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                time: currentTime,
+                lat: location[0],
+                lon: location[1]
+            })
+        })
+        .then(resp => resp.json())
+        .then(json => {
+            setPrediction(json.prediction);
+        })
     }
 
     useEffect(() => {
