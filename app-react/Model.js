2debe99d83e7e2a60020ff9652eee69345333ca2
Aliraza <aliraza.n@live.com>
2021-04-30 15:55:59 -0400

    model component updates current time

--- app-react/Model.js
+++ app-react/Model.js
@@ -1,16 +1,29 @@
 import { Container } from 'native-base';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+const DEFAULT_LAT = '43.643750';
+const DEFAULT_LON = '-79.389983';
+
 const Model = () => {
-    const [location, setLocation] = useState(null);
-    const [currentTime, setCurrentTime] = useState(null);
+    const [location, setLocation] = useState([DEFAULT_LAT, DEFAULT_LON]);
+    const [currentTime, setCurrentTime] = useState('');
     const [prediction, setPrediction] = useState(-1);
+
+    const updateCurrentTime = () => {
+        const now = new Date();
+        const formattedTime = `${now.getHours()}:${now.getMinutes()}`;
+        setCurrentTime(formattedTime);
+    }
+
+    useEffect(() => {
+        updateCurrentTime();
+    })
 
     return (
         <Container>
             <Text>
-                Hello World
+                {currentTime}
             </Text>
         </Container>
     )
